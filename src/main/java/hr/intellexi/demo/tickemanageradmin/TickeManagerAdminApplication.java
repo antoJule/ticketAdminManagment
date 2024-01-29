c13321560ef5855d53ba16769f6f6a5a611c849f
Anto Julardzija <jule@Antos-MacBook-Air.local>
2024-01-29 16:49:51 +0100

    Application starter

---
+++ src/main/java/hr/intellexi/demo/tickemanageradmin/TickeManagerAdminApplication.java
@@ -0,0 +1,13 @@
+package hr.intellexi.demo.tickemanageradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickeManagerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickeManagerAdminApplication.class, args);
+    }
+
+}
